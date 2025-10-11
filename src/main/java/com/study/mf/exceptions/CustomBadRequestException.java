34be45ba140e960aabf04adeea5825cb6ec51fa2
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2025-10-11 10:12:22 -0300

    add CustomBadRequestException

--- src/main/java/com/study/mf/exceptions/CustomBadRequestException.java
+++ src/main/java/com/study/mf/exceptions/CustomBadRequestException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class CustomResourceNotFound extends RuntimeException {
-    public CustomResourceNotFound(String message) {
+public class CustomBadRequestException extends RuntimeException {
+    public CustomBadRequestException(String message) {
         super(message);
     }
 }
--- src/main/java/com/study/mf/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/study/mf/exceptions/CustomExceptionHandler.java
@@ -1,15 +1,53 @@
 package com.study.mf.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
 
 @RestControllerAdvice
-public class CustomExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(CustomResourceNotFound.class)
-    public ResponseEntity<ResponseException> customNotFoundExceptionHandler(){
-        return ResponseEntity.notFound().build();
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseException> genericExceptionHandler(Exception ex, WebRequest request) {
+        return ResponseEntity.internalServerError().body(
+            new ResponseException(
+                Instant.now().toEpochMilli(),
+                ex.getMessage(),
+                request.getDescription(false)
+            )
+        );
+    }
+
+    @ExceptionHandler(CustomResourceNotFoundException.class)
+    public ResponseEntity<ResponseException> customNotFoundExceptionHandler(
+        CustomBadRequestException ex,
+        WebRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+            new ResponseException(
+                Instant.now().toEpochMilli(),
+                ex.getMessage(),
+                request.getDescription(false)
+            )
+        );
+    }
+
+    @ExceptionHandler(CustomBadRequestException.class)
+    public ResponseEntity<ResponseException> customBadRequestExceptionHandler(
+        CustomBadRequestException ex,
+        WebRequest request
+    ) {
+        return ResponseEntity.badRequest().body(
+            new ResponseException(
+                Instant.now().toEpochMilli(),
+                ex.getMessage(),
+                request.getDescription(false)
+            )
+        );
     }
 }
