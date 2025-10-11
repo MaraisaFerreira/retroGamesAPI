b98a9dd003071ef9cb92450aae62266a410ce692
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2025-10-11 12:57:39 -0300

    feat. CORS control access

--- src/main/java/com/study/mf/config/WebContentConfigs.java
+++ src/main/java/com/study/mf/config/WebContentConfigs.java
@@ -1,12 +1,26 @@
-package com.study.mf;
+package com.study.mf.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebContentConfigs implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns}")
+    private String originPatterns;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] origins = originPatterns.split(",");
+
+        registry.addMapping("/**")
+            .allowedOrigins(origins);
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.ignoreAcceptHeader(false)
